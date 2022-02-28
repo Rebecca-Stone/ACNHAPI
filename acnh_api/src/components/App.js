4f20fa54bfd38e10724359bc7f461ae95607c319
Rebecca Stone <you@example.com>
2022-02-27 18:42:33 -0600

    working on writing an outline

--- acnh_api/src/components/App.js
+++ acnh_api/src/components/App.js
@@ -6,9 +6,26 @@ import Animal from "./AnimalCard/Animal";
 import Search from "./Search";
 import "./App.css";
 
+//may need to make initial form values
+
+//may need to make initial form errors
+
+//could disable the button on the form if no search filters are applied
+
 function App() {
+  //a state to keep track of search filters values
+
+  //a state to keep track of errors
+
+  //a state to disable and enable submit button 
+
+  //will need to setFormValues
+
+  //will need a way to enable and disable the button
   return (
     <div>
+      {/* thinking about making a route for a filter form */}
+      {/* thinking about making a route for the favorite animals */}
       <Search />
       <Animal />
     </div>
---
+++ acnh_api/src/components/Filtered.js
@@ -0,0 +1,10 @@
+// this will be a filter area to find animals with certain qualities 
+
+//will need to import link
+
+//will need to pass down some props
+
+//will make an onChange
+//will make an onSubmit & preventDefault
+
+//need to figure out how the form will be displayed
